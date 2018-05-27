b0ca80dc8d7321060928aa39ed013e894afa4693
dorako321 <yamad@daiji.ro>
2018-05-28 06:45:02 +0900

    first commit.

---
+++ imgext.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"io/ioutil"
+	"reflect"
+	"flag"
+	"golang.org/x/crypto/ssh/terminal"
+	"syscall"
+)
+
+var (
+	tbName     = flag.String("tb", "image", "table name (default is image)")
+	pkColumn   = flag.String("pc", "id", "primary key column name (default is id)")
+	dataColumn = flag.String("dc", "data", "binary data column name (default is data)")
+
+	host      = flag.String("h", "localhost", "host name (default is localhost)")
+	port      = flag.String("p", "3306", "port number (default is 3306)")
+	user      = flag.String("u", "root", "user name (default is root)")
+	pass      = flag.String("pw", "password", "password (default is password)")
+	dbName    = flag.String("d", "isucon", "database name (default is isucon)")
+	outputDir = flag.String("o", "./output", "output directory (default is ./output)")
+)
+
+func main() {
+	// パラメーターの取得
+	flag.Parse()
+	// パスワードの取得
+	if *pass == "" {
+		fmt.Print("Password: ")
+		pw, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			*pass = string(pw)
+		}
+	}
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		*user,
+		*pass,
+		*host,
+		*port,
+		*dbName,
+	)
+
+	// connect mysql db
+	db, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %s.", err.Error())
+	}
+	defer db.Close()
+
+	// select
+	// 必要に応じてクエリを修正してください
+	rows, err := db.Query(
+		"SELECT `" + *pkColumn + "`, `" + *dataColumn +
+			"` FROM `" + *tbName + "`")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	defer rows.Close()
+
+	var id string
+	var data []byte
+	for rows.Next() {
+		err = rows.Scan(&id, &data)
+
+		// バイナリデータから拡張子を判定
+		ext := getExtensionName(data)
+		fmt.Println(id, ext)
+		ioutil.WriteFile(*outputDir + "/" + id + ext, data, 0666)
+		os.Exit(0)
+	}
+}
+
+func getExtensionName(data []byte) string {
+	pngHeader := []byte{137, 80, 78, 71, 13, 10, 26, 10}
+	jpgHeader := []byte{255, 216}
+	gifHeader := []byte{71, 73, 70}
+
+	if reflect.DeepEqual(data[:len(pngHeader)], pngHeader) {
+		return ".png"
+	}
+	if reflect.DeepEqual(data[:len(jpgHeader)], jpgHeader) {
+		return ".jpg"
+	}
+	if reflect.DeepEqual(data[:len(gifHeader)], jpgHeader) {
+		return ".gif"
+	}
+	return ".unknown"
+}
---
+++ modules/Binary/Binary.go
@@ -0,0 +1,20 @@
+package Binary
+
+import "reflect"
+
+func getExtensionName(data []byte) string {
+	pngHeader := []byte{137, 80, 78, 71, 13, 10, 26, 10}
+	jpgHeader := []byte{255, 216}
+	gifHeader := []byte{71, 73, 70}
+
+	if reflect.DeepEqual(data[:len(pngHeader)], pngHeader) {
+		return ".png"
+	}
+	if reflect.DeepEqual(data[:len(jpgHeader)], jpgHeader) {
+		return ".jpg"
+	}
+	if reflect.DeepEqual(data[:len(gifHeader)], jpgHeader) {
+		return ".gif"
+	}
+	return ".unknown"
+}
